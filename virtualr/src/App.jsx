c99c1f2cb6d6676d20805463c6ec1b293ab5e0d5
narendra-patil1509 <np738587@gmail.com>
2024-08-13 16:51:04 +0530

    Update App.jsx

--- virtualr/src/App.jsx
+++ virtualr/src/App.jsx
@@ -1,9 +1,17 @@
+import  Workflow  from "./components/Woorkflow";
+import FeatureSection from "./components/FeatureSection";
+import HeroSection from "./components/HeroSection";
 import Navbar from "./components/Navbar";
 
 const App = () => {
   return (
     <>
       <Navbar />
+      <div className="max-w-7xl mx-auto pt-20 px-6">
+        <HeroSection />
+        <FeatureSection />
+        <Workflow />
+      </div>
     </>
   )
 }
