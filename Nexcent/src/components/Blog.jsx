b7181c0f952b3d4750765b7500dd485533801683
narendra-patil1509 <np738587@gmail.com>
2024-08-24 18:49:15 +0530

    blog object updated

--- Nexcent/src/components/Blog.jsx
+++ Nexcent/src/components/Blog.jsx
@@ -3,14 +3,14 @@ import React from 'react'
 const Blog = () => {
 
     const blogs = [
-        {id:1,title:"CReating Stremalined Safeguarding Processes with OneRen", image:"/src/assets/blog1.png"},
-        {id:2,title:"CReating Stremalined Safeguarding Processes with OneRen", image:"/src/assets/blog1.png"},
-        {id:3,title:"CReating Stremalined Safeguarding Processes with OneRen", image:"/src/assets/blog1.png"},
+        {id:1,title:"Creating Stremalined Safeguarding Processes with OneRen", image:"/src/assets/computer1.jpg"},
+        {id:2,title:"Creating Stremalined Safeguarding Processes with OneRen", image:"/src/assets/computer2.jpg"},
+        {id:3,title:"Creating Stremalined Safeguarding Processes with OneRen", image:"/src/assets/computer3.jpg"},
     ];
   return (
     <div className='px-4 lg:px-14 max-w-screen-2xl mx-auto my-12' id='faq'>
         <div className='text-center md:w-1/2 mx-auto'>
-            <h2 className='text-4xl text-neutralDGrey font-semibold mb-4'>The unseen of spending three years at Pixelgrade</h2>
+            <h2 className='text-4xl text-neutralDGrey font-semibold mb-4'>The blog of spending three years at Pixelgrade</h2>
             <p className='text-sm text-neutralDGrey mb-8 md:w-3/4 mx-auto'> Lorem ipsum dolor sit amet consectetur adipisicing elit. Cumque velit consequuntur soluta eos sunt quae quidem nihil id.</p>
         </div>
 
@@ -18,11 +18,11 @@ const Blog = () => {
         <div className='grid lg:grid-cols-3 sm:grid-cols-2 grid-cols-1 gap-8 item-center justify-between'>
             {
                 blogs.map(blog => <div key={blog.id} className='mx-auto relative mb-12 cursor-pointer'>
-                    <img src="#" className='hover:scale-95 transition-all duration-300' alt="" />
+                    <img src={blog.image} className='hover:scale-95 transition-all duration-300' alt="" />
                     <div className='text-center px-4 py-8 bg-white shadow-lg rounde-md md:w-3/4 mx-auto absolute left-0 right-0 -bottom-12'>
                         <h3 className='mb-3 text-neutralDGrey font-semibold'>{blog.title}</h3>
                         <div className='flex items-center justify-center gap-8'>
-                                    <a href="/" className='font-bold text-brandPrimary hover:text-neutral-700'>
+                                    <a href={blogs.image} className='font-bold text-brandPrimary hover:text-neutral-700'>
                                         ReadMore{" "}
                                         <svg
                                             xmlns="http://www.w3.org/200/svg"
