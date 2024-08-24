b4b4bb4e89c654171bc1b6ca727f0dfe9627a493
narendra-patil1509 <np738587@gmail.com>
2024-08-24 18:50:12 +0530

    asset added

--- Nexcent/src/components/Products.jsx
+++ Nexcent/src/components/Products.jsx
@@ -1,5 +1,5 @@
 import React from 'react'
-// import aboutImg from '../assets/mobile-login.png';
+import aboutImg from '../assets/ComputerDesign.jpg';
 
 const Products = () => {
   return (
@@ -8,8 +8,7 @@ const Products = () => {
         <div className="px-4 lg:px-14 max-w-screen-2xl mx-auto my-8" id='product'>
             <div className='md:w-11/12 mx-auto flex flex-col md:flex-row justify-between items-center gap-12'>
                 <div>
-                {/* {aboutImg} */}
-                    <img src="#" alt="" />
+                    <img src={aboutImg} alt="" />
                 </div>
                 <div className='md:w-3/5 mx-auto'>
                     <h2 className='text-4xl text-neutralDGrey font-semibold mb-4 md:w-4/5'>How to design your site footer like we did</h2>
