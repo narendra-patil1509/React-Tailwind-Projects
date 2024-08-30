869d706f765afb56f628f9743e6f7a5290590363
narendra-patil1509 <np738587@gmail.com>
2024-08-30 16:38:26 +0530

    Framer motion added

--- Restaurant/src/components/Expertise.jsx
+++ Restaurant/src/components/Expertise.jsx
@@ -1,4 +1,22 @@
 import {CUSINES} from "../constants"
+import {motion} from "framer-motion"
+
+const containerVariants = {
+    hidden: {opacity:0},
+    show: {
+        opacity:1,
+        transition:{
+            staggerChildren: 1
+        }
+    }
+}
+
+const ItemVariants = {
+    hidden: { opcity:0, y:20},
+    show: { opacity:1, y:0, transition: {
+        duration:0.8
+    }}
+}
 
 const Expertise = () => {
   return (
@@ -6,10 +24,15 @@ const Expertise = () => {
         <h2 className='my-8 text-center text-3xl tracking-tighter lg:text-4xl'>
             Our Expertise
         </h2>
-        <div className='container mx-auto px-4'>
+        <motion.div
+            initial="hidden"
+            whileInView="show"
+            variants={containerVariants}
+            className='container mx-auto px-4'>
             {
                 CUSINES.map((cusine, index) => (
-                    <div key={index} className='flex items-center border-b-4  border-dotted border-neutral-700/40 py-2'>
+                    <motion.div key={index} className='flex items-center border-b-4  border-dotted border-neutral-700/40 py-2'
+                        variants={ItemVariants}>
                         <div className="flex-shrink-0 pr-8 text-2xl">{cusine.number}</div>
                         <div className="w-1/3 flex-shrink-0">
                             <img src={cusine.image} alt={cusine.title} className="h-auto rounded-3xl" />
@@ -24,10 +47,10 @@ const Expertise = () => {
                                 {cusine.description}
                             </p>
                         </div>
-                    </div>
+                    </motion.div>
                 ))
             }
-        </div>
+        </motion.div>
     </section>
   )
 }
