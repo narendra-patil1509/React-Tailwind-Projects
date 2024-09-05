547b2e77ddc0deb67c184f7c9a8861b927e3b49e
narendra-patil1509 <np738587@gmail.com>
2024-09-05 18:06:07 +0530

    add steps section

--- ToDesktop/src/components/Hero.jsx
+++ ToDesktop/src/components/Hero.jsx
@@ -23,6 +23,8 @@ import asset23 from "../assets/asset 23.png";
 import asset24 from "../assets/asset 24.png";
 import asset25 from "../assets/asset 25.png";
 import asset26 from "../assets/asset 26.png";
+
+import asset66 from "../assets/asset 66.svg";
 import { useEffect } from "react";
 
 const Hero = () => {
@@ -250,6 +252,119 @@ const Hero = () => {
                             <span className="text-[12px] md:text-[16px] font-semibold">Unbounce</span>
                         </div>
                     </div>
+                </div>
+            </div>
+        </div>
+
+        {/* step by step */}
+        <div id="steps" className="flex flex-col gap-6 px-6 py-12 max-w-7xl mt-16 mx-auto lg:px-8 lg:mt-32">
+            <h2 className="text-5xl sm:font-semibold mb-14">How it Works</h2>
+            <div id="step1" className="rounded-xl border px-8 py-12 flex flex-col lg:flex-row">
+                <div className="flex flex-col gap-6 lg:w-1/2">
+                    <span className="border border-yellow-300 bg-yellow-50 text-yellow-800 w-fit px-3 py-1 rounded-lg font-medium font-display">Step1</span>
+                    <h3 className="text-4xl">Bootstrap straight from your web app</h3>
+                    <p className="text-lg font-light">Copy and paste your web app url into ToDesktop. Customise your app design, app icon and window frame UI with no code.</p>
+                    <ul className="grid grid-cols-2 gap-2">
+                        <li className="flex gap-4 items-center">
+                            <i className="fa-solid fa-check text-gray-500"></i>
+                            <a className="text-lg font-light border-b border-black border-opacity-30 hover:border-opacity-80" href="#">Multiple windows</a>
+                        </li>
+                        <li className="flex gap-4 items-center">
+                            <i className="fa-solid fa-check text-gray-500"></i>
+                            <a className="text-lg font-light border-b border-black border-opacity-30 hover:border-opacity-80" href="#">Offline support</a>
+                        </li>
+                        <li className="flex gap-4 items-center">
+                            <i className="fa-solid fa-check text-gray-500"></i>
+                            <a className="text-lg font-light border-b border-black border-opacity-30 hover:border-opacity-80" href="#">Launch on startup</a>
+                        </li>
+                        <li className="flex gap-4 items-center">
+                            <i className="fa-solid fa-check text-gray-500"></i>
+                            <a className="text-lg font-light border-b border-black border-opacity-30 hover:border-opacity-80" href="#">Menubar/tray menus</a>
+                        </li>
+                        <li className="flex gap-4 items-center">
+                            <i className="fa-solid fa-check text-gray-500"></i>
+                            <a className="text-lg font-light border-b border-black border-opacity-30 hover:border-opacity-80" href="#">Customisable menus</a>
+                        </li>
+                        <li className="flex gap-4 items-center">
+                            <i className="fa-solid fa-check text-gray-500"></i>
+                            <a className="text-lg font-light border-b border-black border-opacity-30 hover:border-opacity-80" href="#">Tabs (Mac only)</a>
+                        </li>
+                    </ul>
+                </div>
+                <div className="pt-12">
+                    <img src={asset66} alt="" />
+                </div>
+            </div>
+            <div id="step2" className="rounded-xl border px-8 py-12 flex flex-col lg:flex-row">
+                <div className="flex flex-col gap-6 lg:w-1/2">
+                    <span className="border border-yellow-300 bg-yellow-50 text-yellow-800 w-fit px-3 py-1 rounded-lg font-medium font-display">Step2</span>
+                    <h3 className="text-4xl">Bootstrap straight from your web app</h3>
+                    <p className="text-lg font-light">Copy and paste your web app url into ToDesktop. Customise your app design, app icon and window frame UI with no code.</p>
+                    <ul className="grid grid-cols-2 gap-2">
+                        <li className="flex gap-4 items-center">
+                            <i className="fa-solid fa-check text-gray-500"></i>
+                            <a className="text-lg font-light border-b border-black border-opacity-30 hover:border-opacity-80" href="#">Multiple windows</a>
+                        </li>
+                        <li className="flex gap-4 items-center">
+                            <i className="fa-solid fa-check text-gray-500"></i>
+                            <a className="text-lg font-light border-b border-black border-opacity-30 hover:border-opacity-80" href="#">Offline support</a>
+                        </li>
+                        <li className="flex gap-4 items-center">
+                            <i className="fa-solid fa-check text-gray-500"></i>
+                            <a className="text-lg font-light border-b border-black border-opacity-30 hover:border-opacity-80" href="#">Launch on startup</a>
+                        </li>
+                        <li className="flex gap-4 items-center">
+                            <i className="fa-solid fa-check text-gray-500"></i>
+                            <a className="text-lg font-light border-b border-black border-opacity-30 hover:border-opacity-80" href="#">Menubar/tray menus</a>
+                        </li>
+                        <li className="flex gap-4 items-center">
+                            <i className="fa-solid fa-check text-gray-500"></i>
+                            <a className="text-lg font-light border-b border-black border-opacity-30 hover:border-opacity-80" href="#">Customisable menus</a>
+                        </li>
+                        <li className="flex gap-4 items-center">
+                            <i className="fa-solid fa-check text-gray-500"></i>
+                            <a className="text-lg font-light border-b border-black border-opacity-30 hover:border-opacity-80" href="#">Tabs (Mac only)</a>
+                        </li>
+                    </ul>
+                </div>
+                <div className="pt-12">
+                    <img src={asset66} alt="" />
+                </div>
+            </div>
+            <div id="step3" className="rounded-xl border px-8 py-12 flex flex-col lg:flex-row">
+                <div className="flex flex-col gap-6 lg:w-1/2">
+                    <span className="border border-yellow-300 bg-yellow-50 text-yellow-800 w-fit px-3 py-1 rounded-lg font-medium font-display">Step3</span>
+                    <h3 className="text-4xl">Bootstrap straight from your web app</h3>
+                    <p className="text-lg font-light">Copy and paste your web app url into ToDesktop. Customise your app design, app icon and window frame UI with no code.</p>
+                    <ul className="grid grid-cols-2 gap-2">
+                        <li className="flex gap-4 items-center">
+                            <i className="fa-solid fa-check text-gray-500"></i>
+                            <a className="text-lg font-light border-b border-black border-opacity-30 hover:border-opacity-80" href="#">Multiple windows</a>
+                        </li>
+                        <li className="flex gap-4 items-center">
+                            <i className="fa-solid fa-check text-gray-500"></i>
+                            <a className="text-lg font-light border-b border-black border-opacity-30 hover:border-opacity-80" href="#">Offline support</a>
+                        </li>
+                        <li className="flex gap-4 items-center">
+                            <i className="fa-solid fa-check text-gray-500"></i>
+                            <a className="text-lg font-light border-b border-black border-opacity-30 hover:border-opacity-80" href="#">Launch on startup</a>
+                        </li>
+                        <li className="flex gap-4 items-center">
+                            <i className="fa-solid fa-check text-gray-500"></i>
+                            <a className="text-lg font-light border-b border-black border-opacity-30 hover:border-opacity-80" href="#">Menubar/tray menus</a>
+                        </li>
+                        <li className="flex gap-4 items-center">
+                            <i className="fa-solid fa-check text-gray-500"></i>
+                            <a className="text-lg font-light border-b border-black border-opacity-30 hover:border-opacity-80" href="#">Customisable menus</a>
+                        </li>
+                        <li className="flex gap-4 items-center">
+                            <i className="fa-solid fa-check text-gray-500"></i>
+                            <a className="text-lg font-light border-b border-black border-opacity-30 hover:border-opacity-80" href="#">Tabs (Mac only)</a>
+                        </li>
+                    </ul>
+                </div>
+                <div className="pt-12">
+                    <img src={asset66} alt="" />
                 </div>
             </div>
         </div>
